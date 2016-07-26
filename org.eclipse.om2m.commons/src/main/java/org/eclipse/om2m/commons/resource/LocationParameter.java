e636425e301934ad88de1a284d993f4028ce5513
SkingXQ <skingxq@gmail.com>
2016-07-26 10:22:59 -0400

    location

---
+++ org.eclipse.om2m.commons/src/main/java/org/eclipse/om2m/commons/resource/LocationParameter.java
@@ -0,0 +1,243 @@
+// Author: sking
+
+
+package org.eclipse.om2m.commons.resource;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.datatype.Duration;
+
+
+/**
+ * <p>
+ * Java class for anonymous complex type.
+ * 
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;extension base="{http://www.onem2m.org/xml/protocols}announceableResource">
+ *       &lt;sequence>
+ *         &lt;element name="locationTargetID" type="{http://www.onem2m.org/xml/protocols}nodeID" minOccurs="0"/>
+ *         &lt;element name="locationServer" type="{http://www.w3.org/2001/XMLSchema}anyURI" minOccurs="0"/>
+ *         &lt;element name="locationContainerID" type="{http://www.w3.org/2001/XMLSchema}anyURI" minOccurs="0"/>
+ *         &lt;element name="locationName" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="locationStatus" type="{http://www.onem2m.org/xml/protocols}status"/>
+ *         &lt;choice minOccurs="0">
+ *           &lt;element name="childResource" type="{http://www.onem2m.org/xml/protocols}childResourceRef" maxOccurs="unbounded"/>
+ *           &lt;choice maxOccurs="unbounded">
+ *             &lt;element ref="{http://www.onem2m.org/xml/protocols}subscription"/>
+ *           &lt;/choice>
+ *         &lt;/choice>
+ *       &lt;/sequence>
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = { "locationTargetID", "locationServer", 
+                "locationContainerID", "locationName", 
+                "locationStatus", "childResource",
+                "subscription" })
+@XmlRootElement(name = "locationPolicy")
+public class LocationPolicy extends AnnounceableResource {
+
+
+        @XmlJavaTypeAdapter(CollapsedStringAdapter.class)
+        protected String locationTargetID;
+        @XmlSchemaType(name = "anyURI")
+        protected String locationServer;
+        @XmlSchemaType(name = "anyURI")
+        protected String locationContainerID;
+        protected String locationName;
+        @XmlElement(required = true)
+        protected BigInteger locationStatus;
+        protected List<ChildResourceRef> childResource;
+        @XmlElement(namespace = "http://www.onem2m.org/xml/protocols")
+        protected List<Subscription> subscription;
+
+        /**
+         * Gets the value of the locationTargetID property.
+         * 
+         * @return possible object is {@link String }
+         * 
+         */
+        public String getLocationTargetID() {
+                return locationTargetID;
+        }
+
+        /**
+         * Sets the value of the locationTargetID property.
+         * 
+         * @param value
+         *            allowed object is {@link String }
+         * 
+         */
+        public void setLocationTargetID(String value) {
+                this.locationTargetID = value;
+        }
+
+        /**
+         * Gets the value of the locationServer property.
+         * 
+         * @return possible object is {@link String }
+         * 
+         */
+        public String getLocationServer() {
+                return locationServer;
+        }
+
+        /**
+         * Sets the value of the locationServer property.
+         * 
+         * @param value
+         *            allowed object is {@link String }
+         * 
+         */
+        public void setLocationServer(String value) {
+                this.locationServer = value;
+        }
+
+        /**
+         * Gets the value of the locationContainerID property.
+         * 
+         * @return possible object is {@link String }
+         * 
+         */
+        public String getLocationContainerID() {
+                return locationContainerID;
+        }
+
+        /**
+         * Sets the value of the locationContainerID property.
+         * 
+         * @param value
+         *            allowed object is {@link String }
+         * 
+         */
+        public void setLocationContainerID(String value) {
+                this.locationContainerID = value;
+        }
+
+        /**
+         * Gets the value of the locationContainerName property.
+         * 
+         * @return possible object is {@link String }
+         * 
+         */
+        public String getLocationName() {
+                return locationContainerName;
+        }
+
+        /**
+         * Sets the value of the locationContainerName property.
+         * 
+         * @param value
+         *            allowed object is {@link String }
+         * 
+         */
+        public void setLocationName(String value) {
+                this.locationContainerName = value;
+        }
+
+        /**
+         * Gets the value of the locationStatus property.
+         * 
+         * @return possible object is {@link BigInteger }
+         * 
+         */
+        public BigInteger getLocationStatus() {
+                return locationStatus;
+        }
+
+        /**
+         * Sets the value of the locationStatus property.
+         * 
+         * @param value
+         *            allowed object is {@link BigInteger }
+         * 
+         */
+        public void setLocationStatus(BigInteger value) {
+                this.locationStatus = value;
+        }
+
+        /**
+         * Gets the value of the childResource property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list, not a
+         * snapshot. Therefore any modification you make to the returned list will
+         * be present inside the JAXB object. This is why there is not a
+         * <CODE>set</CODE> method for the childResource property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * 
+         * <pre>
+         * getChildResource().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link ChildResourceRef }
+         * 
+         * 
+         */
+        public List<ChildResourceRef> getChildResource() {
+                if (childResource == null) {
+                        childResource = new ArrayList<ChildResourceRef>();
+                }
+                return this.childResource;
+        }
+
+        /**
+         * Gets the value of the subscription property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list, not a
+         * snapshot. Therefore any modification you make to the returned list will
+         * be present inside the JAXB object. This is why there is not a
+         * <CODE>set</CODE> method for the subscription property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * 
+         * <pre>
+         * getSubscription().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link Subscription }
+         * 
+         * 
+         */
+        public List<Subscription> getSubscription() {
+                if (subscription == null) {
+                        subscription = new ArrayList<Subscription>();
+                }
+                return this.subscription;
+        }
+
+
+}
---
+++ org.eclipse.om2m.core/src/main/java/org/eclipse/om2m/core/controller/LocationController.java
@@ -0,0 +1,77 @@
+// Author : sking
+
+package org.eclipse.om2m.core.controller;
+
+
+import java.util.List;
+
+import org.eclipse.om2m.commons.constants.ConsistencyStrategy;
+import org.eclipse.om2m.commons.constants.Constants;
+import org.eclipse.om2m.commons.constants.MemberType;
+import org.eclipse.om2m.commons.constants.MimeMediaType;
+import org.eclipse.om2m.commons.constants.Operation;
+import org.eclipse.om2m.commons.constants.ResourceStatus;
+import org.eclipse.om2m.commons.constants.ResourceType;
+import org.eclipse.om2m.commons.constants.ResponseStatusCode;
+import org.eclipse.om2m.commons.constants.ShortName;
+import org.eclipse.om2m.commons.entities.AccessControlPolicyEntity;
+import org.eclipse.om2m.commons.entities.AeEntity;
+import org.eclipse.om2m.commons.entities.CSEBaseEntity;
+import org.eclipse.om2m.commons.entities.GroupEntity;
+import org.eclipse.om2m.commons.entities.RemoteCSEEntity;
+import org.eclipse.om2m.commons.entities.ResourceEntity;
+import org.eclipse.om2m.commons.entities.SubscriptionEntity;
+import org.eclipse.om2m.commons.exceptions.BadRequestException;
+import org.eclipse.om2m.commons.exceptions.ConflictException;
+import org.eclipse.om2m.commons.exceptions.NotImplementedException;
+import org.eclipse.om2m.commons.exceptions.NotPermittedAttrException;
+import org.eclipse.om2m.commons.exceptions.Om2mException;
+import org.eclipse.om2m.commons.exceptions.ResourceNotFoundException;
+import org.eclipse.om2m.commons.resource.Group;
+import org.eclipse.om2m.commons.resource.RequestPrimitive;
+import org.eclipse.om2m.commons.resource.ResponsePrimitive;
+import org.eclipse.om2m.commons.utils.Util.DateUtil;
+import org.eclipse.om2m.core.datamapper.DataMapperSelector;
+import org.eclipse.om2m.core.entitymapper.EntityMapperFactory;
+import org.eclipse.om2m.core.notifier.Notifier;
+import org.eclipse.om2m.core.persistence.PersistenceService;
+import org.eclipse.om2m.core.router.Patterns;
+import org.eclipse.om2m.core.urimapper.UriMapper;
+import org.eclipse.om2m.core.util.ControllerUtil;
+import org.eclipse.om2m.core.util.ControllerUtil.UpdateUtil;
+import org.eclipse.om2m.core.util.GroupUtil;
+import org.eclipse.om2m.persistence.service.DAO;
+import org.eclipse.om2m.persistence.service.DBService;
+import org.eclipse.om2m.persistence.service.DBTransaction;
+
+
+/**
+ * Controller for Location
+ *
+ */
+public class LocationController extends Controller {
+
+        @Override
+        public ResponsePrimitive doCreate(RequestPrimitive request) {
+                ResponsePrimitive response = new ResponsePrimitive(request);
+
+	
+	}
+
+
+        @Override
+        public ResponsePrimitive doRetrieve(RequestPrimitive request) {
+
+	}
+
+        @Override
+        public ResponsePrimitive doUpdate(RequestPrimitive request) {
+
+	}
+
+
+        @Override
+        public ResponsePrimitive doDelete(RequestPrimitive request) {
+
+	}
+}
