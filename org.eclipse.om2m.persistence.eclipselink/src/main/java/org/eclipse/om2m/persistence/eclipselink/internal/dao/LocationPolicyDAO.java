2b1b53ffd7fb858596109b0962ee3b4bbed086e6
skingxq <skingxq@gmail.com>
2016-08-01 01:51:23 +0800

    LocationPolicyDAO.java

--- org.eclipse.om2m.persistence.eclipselink/src/main/java/org/eclipse/om2m/persistence/eclipselink/internal/dao/LocationPolicyDAO.java
+++ org.eclipse.om2m.persistence.eclipselink/src/main/java/org/eclipse/om2m/persistence/eclipselink/internal/dao/LocationPolicyDAO.java
@@ -18,29 +18,25 @@ import org.eclipse.om2m.persistence.service.DBTransaction;
 
 public class LocationPolicyEntityDAO extends AbstractDAO<LocationPolicyEntity>{
 
-        @Override
-        public LocationPolicyEntity find(DBTransaction dbTransaction, Object id) {
-                DBTransactionJPAImpl transaction = (DBTransactionJPAImpl) dbTransaction;
-                return transaction.getEm().find(LocationPolicyEntity.class, id);
-        }
+    @Override
+    public LocationPolicyEntity find(DBTransaction dbTransaction, Object id) {
+        DBTransactionJPAImpl transaction = (DBTransactionJPAImpl) dbTransaction;
+        return transaction.getEm().find(LocationPolicyEntity.class, id);
+    }
 
-        @Override
-        public void delete(DBTransaction dbTransaction, LocationPolicyEntity resource) {
-                DBTransactionJPAImpl transaction = (DBTransactionJPAImpl) dbTransaction;
-                transaction.getEm().remove(resource);
-                transaction.getEm().getEntityManagerFactory().getCache().evict(CSEBaseEntity.class);
-                //transaction.getEm().getEntityManagerFactory().getCache().evict(AeEntity.class);
-                //transaction.getEm().getEntityManagerFactory().getCache().evict(RemoteCSEEntity.class);
-                //transaction.getEm().getEntityManagerFactory().getCache().evict(AeAnncEntity.class);
-                //transaction.getEm().getEntityManagerFactory().getCache().evict(RemoteCseAnncEntity.class);
-        }
+    @Override
+    public void delete(DBTransaction dbTransaction, LocationPolicyEntity resource) {
+        DBTransactionJPAImpl transaction = (DBTransactionJPAImpl) dbTransaction;
+        transaction.getEm().remove(resource);
+        transaction.getEm().getEntityManagerFactory().getCache().evict(CSEBaseEntity.class);
+    }
 
-        @Override
-        public void update(DBTransaction dbTransaction, LocationPolicyEntity resource) {
-                List<LocationPolicyEntity> lpe = processLocationPolicy(dbTransaction, resource.getLocationPolicyEntities());
-                resource.setLocationPolicyEntities(lpe);
-                super.update(dbTransaction, resource);
-        }
+    @Override
+    public void update(DBTransaction dbTransaction, LocationPolicyEntity resource) {
+        List<LabelEntity> lbls = processLabels(dbTransaction, resource.getLabelsEntities());
+        resource.setLabelsEntities(lbls);
+        super.update(dbTransaction, resource);
+    }   
 
 }
 
