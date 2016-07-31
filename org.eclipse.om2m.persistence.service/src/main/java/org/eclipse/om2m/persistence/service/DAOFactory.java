41d8764c74cde01f7e719d9b708ef23e7497c3aa
skingxq <skingxq@gmail.com>
2016-07-31 22:07:50 +0800

    update daofactor

--- org.eclipse.om2m.persistence.service/src/main/java/org/eclipse/om2m/persistence/service/DAOFactory.java
+++ org.eclipse.om2m.persistence.service/src/main/java/org/eclipse/om2m/persistence/service/DAOFactory.java
@@ -33,7 +33,8 @@ import org.eclipse.om2m.commons.entities.RemoteCSEEntity;
 import org.eclipse.om2m.commons.entities.RequestEntity;
 import org.eclipse.om2m.commons.entities.SubscriptionEntity;
 import org.eclipse.om2m.commons.entities.UriMapperEntity;
-import org.eclipse.om2m.commons.entities.LocationEntity;
+import org.eclipse.om2m.commons.entities.LocationPolicyEntity;
+import org.eclipse.om2m.commons.entities.LocationParameterEntity;
 
 /**
  * Returns the corresponding DAO 
@@ -70,6 +71,8 @@ public interface DAOFactory {
 
 	public abstract DAO<AccessControlOriginatorEntity> getAccessControlOriginatorDAO();
 	
-	public abstract DAO<LocationEntity> getLocationDAO();
+	public abstract DAO<LocationPolicyEntity> getLocationPolicyDAO();
+
+    public abstract DAO<LocationParameterEntity> getLocationParameterDAO();
 }
 
