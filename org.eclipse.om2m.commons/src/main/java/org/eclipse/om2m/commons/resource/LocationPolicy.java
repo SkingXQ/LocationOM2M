417cb718f8b090dcbf6fe3feafd01c6c3dc5e47a
SkingXQ <skingxq@gmail.com>
2016-08-14 21:20:06 +0800

    update locatin poliy resource

--- org.eclipse.om2m.commons/src/main/java/org/eclipse/om2m/commons/resource/LocationPolicy.java
+++ org.eclipse.om2m.commons/src/main/java/org/eclipse/om2m/commons/resource/LocationPolicy.java
@@ -90,12 +90,9 @@ public class LocationPolicy extends AnnounceableResource {
 	@XmlElement(required = true)
 	protected BigInteger locationStatus;
 	protected String locationMethod;
-	protected List<ChildResourceRef> childResource;
-	@XmlElement(namespace = "http://www.onem2m.org/xml/protocols")
-	protected List<Subscription> subscription;
-    /*@XmlElement(name = ShortName.LOCATIONPARAMETER, namespace = "http://www.onem2m.org/xml/protocol
-    s")
-    protected List<LocationParameter> locationParameter;*/
+
+
+
         
         // TODO: check use or not
         @XmlSchemaType(name = "anyURI")
