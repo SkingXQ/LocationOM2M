d61dfe71fa096130d33a03b99628818a4b97275b
SkingXQ <skingxq@gmail.com>
2016-08-21 21:17:16 +0800

    add location policy

--- org.eclipse.om2m.commons/src/main/java/org/eclipse/om2m/commons/resource/LocationPolicy.java
+++ org.eclipse.om2m.commons/src/main/java/org/eclipse/om2m/commons/resource/LocationPolicy.java
@@ -40,6 +40,9 @@ import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import javax.xml.datatype.Duration;
 
+import org.eclipse.om2m.commons.constants.ShortName;
+
+
 /**
  * <p>
  * Java class for anonymous complex type.
@@ -96,6 +99,42 @@ public class LocationPolicy extends AnnounceableResource {
         @XmlSchemaType(name = "anyURI")
         protected String locationContainerID;
         protected String locationContainerName;
+
+
+        @XmlElement(name=ShortName.CHILD_RESOURCE)
+        protected List<ChildResourceRef> childResource;
+
+
+        /**
+         * Gets the value of the childResource property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list, not a
+         * snapshot. Therefore any modification you make to the returned list will
+         * be present inside the JAXB object. This is why there is not a
+         * <CODE>set</CODE> method for the childResource property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * 
+         * <pre>
+         * getChildResource().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link ChildResourceRef }
+         * 
+         * 
+         */
+        public List<ChildResourceRef> getChildResource() {
+                if (childResource == null) {
+                        childResource = new ArrayList<ChildResourceRef>();
+                }
+                return this.childResource;
+        }
+
 
 	/**
 	 * Gets the value of the locationSource property.
